aaa3ceffb3af75288c3bd1f68a57c5c09ec926e7
Ian Andwati <andwatiian@gmail.com>
2022-07-06 12:37:00 +0300

    created the application entry point

---
+++ todo/cli.py
@@ -0,0 +1,28 @@
+from lib2to3.pgen2.token import OP
+from typing import Optional
+
+import typer
+
+from todo import __app_name__, __version__
+
+app = typer.Typer()
+
+
+def _version_callback(value: bool) -> None:
+    if value:
+        typer.echo(f"{__app_name__} v{__version__}")
+        raise typer.Exit()
+
+
+@app.callback()
+def main(
+    version: Optional[bool] = typer.Option(
+        None,
+        "--version",
+        "-v",
+        help="Show the application version and exit",
+        callback=_version_callback,
+        is_eager=True,
+    )
+) -> None:
+    return
